0beb235d2ab099352c6f0f9ba76d7923b248fbb7
Benjamin Staffin <benley@gmail.com>
2019-08-18 23:18:55 -0400

    Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Graphics.Gloss.Interface.Pure.Game
 type Radius = Float
 type Position = (Float, Float)
 
--- | Window dimenaions
+-- | Window dimensions
 width, height :: Int
 width = 400
 height = 400
@@ -15,7 +15,6 @@ height = 400
 windowPosition :: (Int, Int)
 windowPosition = (400, 400)
 
--- | Ball radius
 ballRadius :: Radius
 ballRadius = 10
 
@@ -24,11 +23,14 @@ paddleWidth, paddleHeight :: Float
 paddleWidth = 26
 paddleHeight = 86
 
--- | Game background color
-background :: Color
-background = black
+-- | Game colors
+ballColor, paddleColor, wallColor, bgColor :: Color
 
--- | Number of frames to show per second.
+bgColor = black
+ballColor = dark red
+wallColor = greyN 0.5
+paddleColor = light (light blue)
+
 fps :: Int
 fps = 60
 
@@ -36,7 +38,7 @@ fps = 60
 movementStep :: Float
 movementStep = 5
 
--- | Data describing the state of the pong game.
+-- | Game state
 data PongGame = Game
   { ballLoc :: Position  -- ^ Pong ball (x, y) location.
   , ballVel :: Position  -- ^ Pong ball (x, y) velocity.
@@ -51,7 +53,6 @@ data PongGame = Game
 data ButtonStatus = Buttons
   { playerL_up, playerL_down, playerR_up, playerR_down :: KeyState } deriving Show
 
--- | The starting state for the game of Pong.
 initialState :: PongGame
 initialState = Game
   { ballLoc = (0, 0)
@@ -67,89 +68,90 @@ initialState = Game
               }
   }
 
--- | Update the ball position using its current velocity.
-moveBall :: Float    -- ^ The number of seconds since last update
-         -> PongGame -- ^ The initial game state
-         -> PongGame -- ^ A new game state with an updated ball position
+-- | Update ball position using its velocity
+moveBall :: Float    -- ^ Seconds since last update
+         -> PongGame -- ^ Initial game state
+         -> PongGame -- ^ Updated game state
 moveBall seconds game = game { ballLoc = (x', y') }
   where
-    -- Old locations and velocities.
+    -- Old location and velocity
     (x, y) = ballLoc game
     (vx, vy) = ballVel game
 
-    -- New locations.
+    -- New location
     x' = x + vx * seconds
     y' = y + vy * seconds
 
--- | Convert a game state into a picture.
-render :: PongGame  -- ^ The game state to render.
-       -> Picture   -- ^ A picture of this game state.
-render game =
-  pictures [ball, walls,
-            mkPaddle rose $ playerR game,
-            mkPaddle orange $ playerL game]
+render :: PongGame  -- ^ Game state to render
+       -> Picture   -- ^ A picture of this game state
+render Game{ballLoc, playerL, playerR} =
+  pictures [ ball
+           , walls
+           , mkPaddle rose playerR
+           , mkPaddle orange playerL
+           ]
   where
-    --  The pong ball.
-    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid ballRadius
-    ballColor = dark red
+    ball = uncurry translate ballLoc $ color ballColor $ circleSolid ballRadius
 
-    --  The bottom and top walls.
-    wall :: Float -> Picture
-    wall yOffset =
+    -- Top and bottom walls
+    mkWall :: Float -> Picture
+    mkWall yOffset =
       translate 0 yOffset $
         color wallColor $
           rectangleSolid (fromIntegral width * 0.9) 10
 
-    wallColor = greyN 0.5
-    walls = pictures [wall (fromIntegral height / 2), wall (-fromIntegral height / 2)]
+    walls = pictures [ mkWall (fromIntegral height / 2)
+                     , mkWall (-fromIntegral height / 2)
+                     ]
 
-    --  Make a paddle of a given border and vertical offset.
-    mkPaddle :: Color -> Position -> Picture
-    mkPaddle col (x, y) = pictures
-      [ translate x y $ color col $ rectangleSolid paddleWidth paddleHeight
+    -- Draw a paddle
+    mkPaddle :: Color    -- ^ Border color
+             -> Position -- ^ Paddle position
+             -> Picture
+    mkPaddle borderColor (x, y) = pictures
+      [ translate x y $ color borderColor $ rectangleSolid paddleWidth paddleHeight
       , translate x y $ color paddleColor $ rectangleSolid (paddleWidth * 0.75) (paddleHeight * 0.9)
       ]
 
-    paddleColor = light (light blue)
-
--- | Update the game by moving the ball and bouncing off walls.
-update :: Float -> PongGame -> PongGame
+-- | Update the game: move the ball, detect collisions/bounces/wins, apply button effects
+update :: Float    -- ^ Seconds since last frame
+       -> PongGame -- ^ Pre-update game state
+       -> PongGame -- ^ New game state
 update seconds game
   | paused game = game
   | otherwise = (detectWin . paddleBounce . wallBounce . applyButtonActions . moveBall seconds) game
 
+-- | If someone scores, crash the entire game because fuck it
 detectWin :: PongGame -> PongGame
 detectWin game@Game{ballLoc = (x, _)}
   | x <= -fromIntegral width / 2 = error "RIGHT PLAYER WINS"
-  | x >= fromIntegral width / 2 = error "LEFT PLAYER WINS"
+  | x >=  fromIntegral width / 2 = error "LEFT PLAYER WINS"
   | otherwise = game
 
--- | Given position and radius of the ball, return whether a collision occurred.
+-- | Detect top / bottom wall collision
 wallCollision :: Position -> Bool
 wallCollision (_, y) = topCollision || bottomCollision
   where
     bottomCollision = y - ballRadius <= -(fromIntegral height / 2) + 5
     topCollision    = y + ballRadius >=  (fromIntegral height / 2) - 5
 
--- | Detect a collision with one of the side walls. Upon collisions,
--- update the velocity of the ball to bounce it off the wall.
+-- | Bounce the ball when it hits a wall
 wallBounce :: PongGame -> PongGame
-wallBounce game@Game{ballVel = (vx, vy)} =
+wallBounce game@Game{ballLoc, ballVel = (vx, vy)} =
   game { ballVel = (vx, vy') }
-  where vy' = if wallCollision (ballLoc game)
-              then -vy -- update velocity: switch direction
-              else vy  -- do nothing: return the old velocity unchanged
+  where vy' = if wallCollision ballLoc
+              then -vy -- Bounce: switch direction
+              else vy  -- No change
 
--- | Detect a collision with a paddle. Upon collisions,
--- change the velocity of the ball to bounce it off the paddle.
+-- | Bounce the ball when it hits a paddle
 paddleBounce :: PongGame -> PongGame
-paddleBounce game = game { ballVel = (vx', vy) }
-  where
-    (vx, vy) = ballVel game
-    vx' = if paddleCollision game
-          then -vx
-          else vx
+paddleBounce game@Game{ballVel = (vx, vy)} =
+  game { ballVel = (vx', vy) }
+  where vx' = if paddleCollision game
+              then -vx
+              else vx
 
+-- | Detect paddle collision
 paddleCollision :: PongGame -> Bool
 paddleCollision Game{ ballLoc = (x, y)
                     , playerR = (playerR_x, playerR_y)
@@ -169,18 +171,18 @@ paddleCollision Game{ ballLoc = (x, y)
     topEdgeOfRightPaddle = playerR_y + (paddleHeight / 2)
     bottomEdgeOfRightPaddle = playerR_y - (paddleHeight / 2)
 
--- | Respond to key events.
+-- | Keypress handler
 handleKeys :: Event -> PongGame -> PongGame
 
--- 'r': Reset the ball to the center
+-- 'r': Reset ball to the center
 handleKeys (EventKey (Char 'r') Down _ _) game =
   game { ballLoc = (0, 0) }
 
 -- 'p': Pause the game
-handleKeys (EventKey (Char 'p') Down _ _) game =
-  game { paused = not (paused game) }
+handleKeys (EventKey (Char 'p') Down _ _) game@Game{paused} =
+  game { paused = not paused }
 
--- Keys that can be held down go into Game state
+-- Paddle motion keys go into game state to be applied later
 handleKeys (EventKey key ks _ _) game@Game{buttons} =
   case key of
     Char 'w' -> game {buttons = buttons {playerL_up = ks}}
@@ -189,31 +191,30 @@ handleKeys (EventKey key ks _ _) game@Game{buttons} =
     SpecialKey KeyDown -> game {buttons = buttons {playerR_down = ks}}
     _ -> game
 
--- Do nothing for all other events.
+-- Ignore all other input
 handleKeys _ game = game
 
 -- | Apply effects from held-down buttons
 applyButtonActions :: PongGame -> PongGame
-applyButtonActions = pl_up . pl_dn . pr_up . pr_dn
+applyButtonActions = pL_up . pL_dn . pR_up . pR_dn
   where
-    inBounds y' = abs y' <= fromIntegral height / 2 - paddleHeight / 2
-    pushed k = k == Down
+    inBounds y = abs y <= fromIntegral height / 2 - paddleHeight / 2
 
-    pl_up g@Game{playerL = (x, y), buttons}
-      | pushed (playerL_up buttons)   && inBounds (y+movementStep) = g{playerL = (x, y+movementStep)}
-      | otherwise = g
-    pl_dn g@Game{playerL = (x, y), buttons}
-      | pushed (playerL_down buttons) && inBounds (y-movementStep) = g{playerL = (x, y-movementStep)}
-      | otherwise = g
-    pr_up g@Game{playerR = (x, y), buttons}
-      | pushed (playerR_up buttons)   && inBounds (y+movementStep) = g{playerR = (x, y+movementStep)}
-      | otherwise = g
-    pr_dn g@Game{playerR = (x, y), buttons}
-      | pushed (playerR_down buttons) && inBounds (y-movementStep) = g{playerR = (x, y-movementStep)}
-      | otherwise = g
+    pL_up g@Game{playerL = (x, y), buttons = Buttons{playerL_up=Down}}
+      | inBounds (y+movementStep) = g{playerL = (x, y+movementStep)}
+    pL_up g = g
+    pL_dn g@Game{playerL = (x, y), buttons = Buttons{playerL_down=Down}}
+      | inBounds (y-movementStep) = g{playerL = (x, y-movementStep)}
+    pL_dn g = g
+    pR_up g@Game{playerR = (x, y), buttons = Buttons{playerR_up=Down}}
+      | inBounds (y+movementStep) = g{playerR = (x, y+movementStep)}
+    pR_up g = g
+    pR_dn g@Game{playerR = (x, y), buttons = Buttons{playerR_down=Down}}
+      | inBounds (y-movementStep) = g{playerR = (x, y-movementStep)}
+    pR_dn g = g
 
 window :: Display
 window = InWindow "Pong" (width, height) windowPosition
 
 main :: IO ()
-main = play window background fps initialState render handleKeys update
+main = play window bgColor fps initialState render handleKeys update
